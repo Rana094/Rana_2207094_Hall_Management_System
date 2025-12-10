0b40f786f3233fed1a516a365afc0aa79674082a
Rana094 <ranaislam135790@gmail.com>
2025-12-10 22:58:45 +0600

    Designed Admin Login Page fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.hall_management_system {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens com.example.hall_management_system to javafx.fxml;
     exports com.example.hall_management_system;
