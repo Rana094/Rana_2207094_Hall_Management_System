fd67bd83ee94f55dbd92fee7aadcffe1b5de2dae
Rana094 <ranaislam135790@gmail.com>
2025-12-10 21:03:12 +0600

    FIRST commit

---
+++ src/main/java/com/example/hall_management_system/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.hall_management_system;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
